de8bb30bbd7b77895bbe15631824ced264f20eff
ShreyanshJain105 <shreyanshjain1206@gmail.com>
2025-09-03 18:51:07 +0530

    recovry the bugs

--- src/main/java/com/jobtrek/MyConfig.java
+++ src/main/java/com/jobtrek/MyConfig.java
@@ -12,7 +12,7 @@ public class MyConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(6);
     }
 
     @Bean
